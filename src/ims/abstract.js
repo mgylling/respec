43aaab60dd452edbf0d3d9ef00b5b8ae99d7888e
Andy Miller (IMS) <amiller@imsglobal.org>
2019-05-03 11:09:30 -0700

    Fix typo (and add ts-check)

--- src/ims/abstract.js
+++ src/ims/abstract.js
@@ -1,18 +1,20 @@
-import { pub } from "core/pubsubhub";
-import { toHTMLNode } from "ims/utils";
+//@ts-check
+import { pub } from "../core/pubsubhub";
+import { toHTMLNode } from "../ims/utils";
 
 export const name =  "ims/abstract";
 
-/*
-* Handles checking for the abstract, and inserts a temp one if not present.
-*/
-
+/**
+ * Handles checking for the abstract, and inserts a temp one if not present.
+ * 
+ * @param {*} conf respecConfig
+ */
 export async function run(conf) {
 
   var abstract = document.querySelector("#abstract");
   if(abstract === null) {
       pub("warn", "No abstract found. Consider adding a section element with an "
-      + "id of'abstract', a class of 'introductory' and a h2 child as the first child of the body");
+      + "id of 'abstract', a class of 'introductory' and a h2 child as the first child of the body");
       //insert a temp abstract
       var tempAbstract = toHTMLNode("<section id='abstract' class='introductory remove'><h2>h</h2></section>");
       document.body.prepend(tempAbstract);
