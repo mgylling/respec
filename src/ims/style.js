f42447ff13e32203d81bdf7e92055842291c7477
Andy Miller (IMS) <amiller@imsglobal.org>
2019-05-03 09:34:03 -0700

    Remove new CSS overrides
    Not needed.

--- src/ims/style.js
+++ src/ims/style.js
@@ -1,25 +1,22 @@
 //@ts-check
-// Module ims/style
-// Inserts the CSS that ReSpec uses into the document. Replaces core/style and w3c/style.
-//
-// Inserts two style sheets: 1) the main ReSpec stylesheet, and 2) the IMS stylesheet
-// which has overrides for the standard ReSpec styles plus custom for things like the 
-// "IMS Final Release" status.
-// 
-// This script also performs the other functions provided by core/style and w3c/style.
-//
-// CONFIGURATION
-// - overrideCSSLocation: by default ims-base.css will be loaded from
-//   https://purl.imsglobal.org/spec/ims-base.css
-// - overrideReSpecCSSLocation: by default respec-all.css will be loaded from
-//   https://purl.imsglobal.org/spec/respec-all.css
+/**
+ * Inserts IMS stylesheet and replaces w3c/style.
+ * 
+ * The CSS documents are pulled from known locations so the spec documents
+ * can be located anywhere. Overrides are available via respecConfig.
+ * 
+ * CONFIGURATION
+ * 
+ * - overrideCSSLocation: by default ims-base.css will be loaded from
+ *   https://purl.imsglobal.org/spec/ims-base.css
+ */
 
 import { toKeyValuePairs, linkCSS } from "../core/utils";
 
 export const name = "ims/style";
 
 /**
- * From w3c/style...
+ * From w3c/style
  * 
  * Make a best effort to attach meta viewport at the top of the head.
  * Other plugins might subsequently push it down, but at least we start
@@ -39,34 +36,28 @@ function attachMetaViewport() {
   document.head.insertBefore(meta, document.head.firstChild);
 }
 
+/**
+ * From w3c/style
+ * 
+ * Ignores specStatus and always loads base.css.
+ */
+function linkW3cCSS() {
+  linkCSS(document, "https://www.w3.org/StyleSheets/TR/2016/base.css");
+}
+
+/**
+ * @param {*} conf respecConfig
+ */
 export async function run(conf) {        
 
+  // From w3c/style
   attachMetaViewport();
+  linkW3cCSS();
 
-  // W3C base styles
-  var w3cCssURL = "https://www.w3.org/StyleSheets/TR/2016/base.css";
-
-  // Highlight styles
-  var highlightCssURL = "https://purl.imsglobal.org/spec/github.css";
-  if (conf.overrideHighlightCSSLocation) {
-    highlightCssURL = conf.overrideHighlightCSSLocation;
-  }
-
-  // ReSpec styles
-  var respecCssURL = "https://purl.imsglobal.org/spec/respec2.css" ;
-  if(conf.overrideReSpecCSSLocation) {
-    respecCssURL = conf.overrideReSpecCSSLocation;
-  }
-
-  // IMS overrides
+  // Link to IMS stylesheet
   var cssURL = "https://purl.imsglobal.org/spec/ims-base.css"  
   if(conf.overrideCSSLocation) {
     cssURL = conf.overrideCSSLocation;
   }
-
-  //linkCSS(document, respecCssURL);
-  linkCSS(document, w3cCssURL);
-  linkCSS(document, highlightCssURL);
-  linkCSS(document, respecCssURL);
   linkCSS(document, cssURL);
 }
