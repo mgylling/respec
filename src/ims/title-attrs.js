71479d31ac357e137a9db2be9fbdf4b240e3e4f9
Andy Miller (IMS) <amiller@imsglobal.org>
2019-05-03 15:03:15 -0700

    Expand description for next dev that looks here

--- src/ims/title-attrs.js
+++ src/ims/title-attrs.js
@@ -1,17 +1,32 @@
 //@ts-check
-
 import { pub } from "../core/pubsubhub";
-
 export const name =  "ims/title-attrs";
 
 /**
- * Add title attributes to relevant elements. 
+ * Adds title attributes to internal definition references. When the
+ * reader hovers over the defined term, they will see the definition.
+ * 
+ * If the term is defined in a definition list, the definiton is pulled
+ * from the definition description element. For example,
+ * 
+ * <dl>
+ *   <dt><dfn>Term</dfn></dt>
+ *   <dd>This is my term.</dd>
+ * </dl>
+ * 
+ * If the term is defined outside of a defintion list, the definition is
+ * pulled from the nearest ancestor p, td, li, div, or aside. For example,
+ * 
+ * <p>
+ *   This is my <dfn>Term</dfn>.
+ * <p>
+ * 
+ * @param {*} conf respecConfig
  */
 export async function run(conf) {
     
   if(conf.noTitleAttrs) {
-      return;
-      
+      return;  
   } 
 
   //for now we deal only with a.internalDFN, whose title attr value is 
