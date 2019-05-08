e7e06719821f023ae7cd4c8214fdbb3c487bab7b
andyfmiller <amiller@gmail.com>
2019-05-08 13:06:39 -0700

    Restore modified version

--- js/profile-ims-default.js
+++ js/profile-ims-default.js
@@ -1,4 +1,4 @@
-"use strict";
++"use strict";
 // In case everything else fails, we want the error
 window.addEventListener("error", ev => {
   console.error(ev.error, ev.message, ev);
@@ -33,23 +33,43 @@ define([
   "./core/reindent",
   "./core/location-hash",
   "./core/l10n",
-  "./w3c/defaults",
+// Similar to ims/compute and not used by IMS specs
+//"./w3c/defaults",
   "./core/style",
-  "./w3c/style",
+// Replaced by ims/style
+//"./w3c/style",
+  "./ims/style",
+  "./ims/config",
+  "./ims/compute",
+  "./ims/transclude",
   "./w3c/l10n",
-  "./core/github",
+// Adds GitHub links to front matter (not used by IMS specs)
+//"./core/github",
   "./core/data-include",
   "./core/markdown",
-  "./w3c/headers",
-  "./w3c/abstract",
+// New order: run id-headers after markdown and before post-markdown
+  "./core/id-headers", 
+// Run after id-headers so abstract is identified
+  "./ims/post-markdown",
+// W3C frontmatter is not used by IMS
+//"./w3c/headers",
+// Replaced by ims/abstract
+//"./w3c/abstract",
+  "./ims/abstract",
   "./core/data-transform",
   "./core/data-abbr",
+  "./ims/inlines",
   "./core/inlines",
-  "./w3c/conformance",
+// Replaced by ims/conformance
+//"./w3c/conformance",
+  "./ims/conformance",
+  "./ims/biblio",
   "./core/dfn",
   "./core/pluralize",
   "./core/examples",
-  "./core/issues-notes",
+// Replaced by ims/admonitions
+//"./core/issues-notes",
+  "./ims/admonitions",
   "./core/requirements",
   "./core/best-practices",
   "./core/figures",
@@ -59,27 +79,38 @@ define([
   "./core/webidl-index",
   "./core/link-to-dfn",
   "./core/render-biblio",
-  "./core/contrib",
+// Replaced by ims/contributors
+//"./core/contrib",
+  "./ims/contributors",
   "./core/fix-headers",
   "./core/structure",
   "./core/informative",
-  "./core/id-headers",
+// Moved id-headers up in order
+//"./core/id-headers",
   "./core/caniuse",
-  "./core/mdn-annotation",
   "./ui/save-html",
   "./ui/search-specref",
   "./ui/dfn-list",
   "./ui/about-respec",
   "./core/seo",
-  "./w3c/seo",
+// Replaced by ims/seo
+//"./w3c/seo",
+  "./ims/seo",
   "./core/highlight",
   "./core/webidl-clipboard",
-  "./core/data-tests",
+// Testing disabled in this profile
+//"./core/data-tests",
   "./core/list-sorter",
   "./core/highlight-vars",
-  "./core/data-type",
   "./core/algorithms",
-  "./core/anchor-expander",
+  "./ims/boilerplate",
+  "./ims/cleanBody",
+  "./ims/title-attrs",
+  "./ims/link-here",
+  "./ims/scripts",
+  // Not working...disable until fixed
+  //"./ims/tooltips",
+  "./ims/comments",
   /* Linter must be the last thing to run */
   "./core/linter",
 ], (runner, { ui }, ...plugins) => {
