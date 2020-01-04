95bffb42e3b2b14a5afab1c70d7a4e62015d7531
andyfmiller <theafmiller@gmail.com>
2020-01-04 10:44:15 -0800

    Remove unused IMS example files

--- ims-examples/shared/abstract.js
+++
@@ -1,6 +0,0 @@
-var t1 = `<section id="abstract" class="introductory"> 
-<h2>Abstract</h2>
-<p>The abstract is a 1-3 paragraph highest level intro summary of the purpose of 
-the specification.  The same text is typically shared between the Specification, 
-Certification Guide and Implementation Guide documents.</p>
-</section>`;
